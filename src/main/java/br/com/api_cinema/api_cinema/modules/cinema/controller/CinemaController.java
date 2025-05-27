a5bf10fd83c9f6d377d9e5b97c6d6505a51eb626
cau4alves <cau4alves@gmail.com>
2025-05-27 19:35:37 -0300

    Tratamento de erro

--- src/main/java/br/com/api_cinema/api_cinema/modules/cinema/controller/CinemaController.java
+++ src/main/java/br/com/api_cinema/api_cinema/modules/cinema/controller/CinemaController.java
@@ -3,6 +3,7 @@ package br.com.api_cinema.api_cinema.modules.cinema.controller;
 import br.com.api_cinema.api_cinema.modules.cinema.model.CinemaModel;
 import br.com.api_cinema.api_cinema.modules.cinema.service.CinemaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,12 @@ public class CinemaController {
     private CinemaService cinemaService;
 
     @PostMapping("/create")
-    public CinemaModel create(@RequestBody CinemaModel cinemaModel) {
-        return this.cinemaService.execute(cinemaModel);
+    public ResponseEntity<Object> create(@RequestBody CinemaModel cinemaModel) {
+        try {
+            var result = this.cinemaService.execute(cinemaModel);
+            return ResponseEntity.ok().body(result);
+        } catch(Exception ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
     }
 }
