edd65022f43e164be6fc6fa3cafa30c79be18631
cau4alves <cau4alves@gmail.com>
2025-05-27 20:56:08 -0300

    Configurações do user

---
+++ src/main/java/br/com/api_cinema/api_cinema/modules/user/model/UserModel.java
@@ -0,0 +1,35 @@
+package br.com.api_cinema.api_cinema.modules.user.model;
+
+import br.com.api_cinema.api_cinema.modules.film.model.FilmModel;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "user")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @JoinColumn(name = "id_filme", insertable = false, updatable = false)
+    @OneToMany
+    private FilmModel filmModel;
+
+    @Column(name = "id_filme")
+    @Nullable
+    private Integer idFilm;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/br/com/api_cinema/api_cinema/modules/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.api_cinema.api_cinema.modules.user.repository;
+
+import br.com.api_cinema.api_cinema.modules.user.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
