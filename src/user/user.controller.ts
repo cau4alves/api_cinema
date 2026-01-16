08bfd3045dc11ae90188e98786f1f88b93b56c06
cau4alves <cau4alves@gmail.com>
2026-01-16 13:44:58 -0300

    feat: list all and find one

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
+
+    @Get()
+    async listAll() {
+        return await this.userService.listAll()
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return await this.userService.findOne(id)
+    }
 
     @Post()
     async create(@Body() body: CreateUserDTO) {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDTO } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
     constructor(private prisma: PrismaService) {}
+
+    async listAll() {
+        const users = await this.prisma.user.findMany()
+        return users
+    }
+
+    async findOne(id: string) {
+        const user = await this.prisma.user.findFirst({
+            where: {
+                id: id
+            }
+        })
+
+        if(!user) {
+            throw new NotFoundException('Nenhum usuário encontrado')
+        }
+
+        return user
+    }
 
     async createUser(body: CreateUserDTO) {
         const user = await this.prisma.user.create({
